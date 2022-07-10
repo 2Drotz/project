0afafaef58856e0fb9772006517f0ad12d6bd21b
Dima <dima@gmail.com>
2022-07-10 17:36:48 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// const result = confirm('Are your here?');
+
+// console.log(result);
+
+let one = 10,
+   two = 10;
+
+console.log(one++);
+console.log(--two);
+console.log(100 % 3);
+
+
+console.log(2 * 10 === 20);
